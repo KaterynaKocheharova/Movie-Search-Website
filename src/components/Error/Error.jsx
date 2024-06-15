ec0cb70bc57ee845a3ee3fc18aa33c71a6a6f970
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-06-15 16:42:39 +0300

    add loader

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,6 +1,8 @@
+import css from "./Error.module.css"
+
 const Error = ({ error }) => {
   return (
-    <p>
+    <p className={css.error}>
       Oops. Something went wrong. Check your internet connection or try again
       later. {error}
     </p>
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,15 @@
+import css from "./Loader.module.css";
+import { Audio } from 'react-loader-spinner'
+;
+
 const Loader = () => {
-  return <p>Loading...</p>;
+  return <p className={css.loader}>Loading... <Audio
+  height="30"
+  width="30"
+  radius="9"
+  color="red"
+  ariaLabel="loading"
+/></p>;
 };
 
 export default Loader;
