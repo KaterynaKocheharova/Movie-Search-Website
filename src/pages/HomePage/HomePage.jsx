dd3d40e53f8771d92bde33bbc3eefc148651dc1d
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-06-17 17:50:41 +0300

    create hook  useFetchTrendingMovies

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,32 +1,11 @@
-import { useState, useEffect } from "react";
-import { getTrendingMoviesToday } from "../../movies-api";
+import { useFetchTrendingMovies } from "../../hooks";
 import Loader from "../../components/Loader/Loader";
 import Error from "../../components/Error/Error";
 import MovieList from "../../components/MovieList/MovieList";
 import css from "./HomePage.module.css";
 
 const Home = () => {
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const getMoviesData = async () => {
-      setError(null);
-      setLoading(true);
-      try {
-        const moviesData = await getTrendingMoviesToday();
-        setMovies(moviesData.results);
-      } catch (error) {
-        setError(error);
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getMoviesData();
-  }, []);
-
+  const { movies, loading, error } = useFetchTrendingMovies();
   return (
     <>
       <h2 className={css.title}>Trending Today</h2>
